afaa31a818598c5545499e3321cfc3eb808943ce
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-05-08 18:47:11 +0200

    calculadora arreglada

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -6,103 +6,180 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
-  name = 'Aitor';
-  resultado = '';
+  title:String = 'calculadora';
+  name:String = 'Aitor';
+  valor1:string;
+  valor2:string;
+  operador:string;
+  resultado = 0.0;
+  count: boolean = false;
+
+  constructor() {
+    this.valor1 = '';
+    this.valor2 = '';
+    this.operador = '';
+  }
 
 
   //Funciones operaciones basicas
 
-sum() {
-  let num = document.getElementById("+");
-  console.log(num)
+sumar() {
+  if(this.count == false) {
+    this.count = true;
+    this.operador = '+';
+  }
 }
 
-// function substract() {
-//   document.getElementById("input").value += "-";
-// }
-
-// function multiply() {
-//   document.getElementById("input").value += "*";
-// }
-
-// function divide() {
-//   document.getElementById("input").value += "/";
-// }
-
-// // Funcion "="
-// function equal() {
-//   document.getElementById("input").value = eval(
-//     document.getElementById("input").value
-//   );
-// }
-
-// //Borra la pantalla
-// function ce() {
-//   document.getElementById("input").value = "";
-// }
-
-// //Coloca el decimal
-// function decimal() {
-//   document.getElementById("input").value += ".";
-// }
-
-// //Funicones números
-
-// function zero() {
-//   document.getElementById("input").value += "0";
-// }
-
-// function one() {
-//   document.getElementById("input").value += "1";
-// }
-
-// function two() {
-//   document.getElementById("input").value += "2";
-// }
-
-// function three() {
-//   document.getElementById("input").value += "3";
-// }
-
-// function four() {
-//   document.getElementById("input").value += "4";
-// }
-
-// function five() {
-//   document.getElementById("input").value += "5";
-// }
-
-// function six() {
-//   document.getElementById("input").value += "6";
-// }
-
-// function seven() {
-//   document.getElementById("input").value += "7";
-// }
-
-// function eight() {
-//   document.getElementById("input").value += "8";
-// }
-
-// function nine() {
-//   document.getElementById("input").value += "9";
-// }
-getNum() {
-  let num = document.getElementById("num");
-  console.log(num);
-  this.resultado += num;
+restar() {
+  if(this.count == false) {
+    this.count = true;
+    this.operador = '-';
+  }
 }
 
-// function del(){
-//     document.getElementById("resultado").value="";
-// }
+dividir() {
+  if(this.count == false) {
+    this.count = true;
+    this.operador = '/';
+  }
+}
 
-// function igual(){
-//     document.getElementById("resultado").value = eval(document.getElementById("resultado").value);
+multiplicar() {
+  if(this.count == false) {
+    this.count = true;
+    this.operador = '*';
+  }
+}
 
-// }
+delete() {
+  this.resultado = 0
+  this.valor1 = '';
+  this.valor2 = '';
+  this.operador = '';
+  this.count = false;
+}
+//POR HACER
+equal() {
 
+  console.log(this.valor1);
+  console.log(this.operador);
+  console.log(this.valor2);
+
+  switch(this.operador) {
+    case "+":
+      this.resultado = parseInt(this.valor1) + parseInt(this.valor2);
+      break;
+    case "-":
+      this.resultado = parseInt(this.valor1) - parseInt(this.valor2);
+      break;
+    case "/":
+      this.resultado = parseInt(this.valor1) / parseInt(this.valor2);
+      break;
+    case "*":
+      this.resultado = parseInt(this.valor1) * parseInt(this.valor2);
+      break;
+    default:
+      break;
+  }
+  console.log(this.resultado);
+  this.delete();
+}
+
+one() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '1';
+  } else {
+    this.valor1 = this.valor1 + '1';
+  }
+}
+
+two() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '2';
+  } else {
+    this.valor1 = this.valor1 + '2';
+  }
+}
+
+three() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '3';
+  } else {
+    this.valor1 = this.valor1 + '3';
+  }
+}
+
+four() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '4';
+  } else {
+    this.valor1 = this.valor1 + '4';
+  }
+}
+
+five() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '5';
+  } else {
+    this.valor1 = this.valor1 + '5';
+  }
+}
+
+six() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '6';
+  } else {
+    this.valor1 = this.valor1 + '6';
+  }
+}
+
+seven() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '7';
+  } else {
+    this.valor1 = this.valor1 + '7';
+  }
+}
+
+eight() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '8';
+  } else {
+    this.valor1 = this.valor1 + '8';
+  }
+}
+
+nine() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '9';
+  } else {
+    this.valor1 = this.valor1 + '9';
+  }
+}
+
+point() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '.';
+  } else {
+    this.valor1 = this.valor1 + '.';
+  }
+}
+
+zero() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '0';
+  } else {
+    this.valor1 = this.valor1 + '0';
+  }
+}
+
+zeroZero() {
+  if(this.count == true) {
+    this.valor2 = this.valor2 + '00';
+  } else {
+    this.valor1 = this.valor1 + '00';
+  }
+}
 }
 
 
